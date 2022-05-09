b435ba43f6cd11441e753136e58f3719e2c784d3
Keke Wu <kwu46@student.monash.edu>
2022-05-09 15:56:28 +0800

    Set default bar chart + add dependencies for facebook share

--- app/src/main/java/com/example/assignment3/report/ReportActivity.java
+++ app/src/main/java/com/example/assignment3/report/ReportActivity.java
@@ -109,79 +109,82 @@ public class ReportActivity extends DrawerActivity {
         });
 
 
-
+        generateBarChart();
 
         binding.updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (chartType == "Bar Chart"){
-                    binding.barChart.setVisibility(View.VISIBLE);
-                    binding.pieChart.setVisibility(View.INVISIBLE);
-                    List<BarEntry> barEntries = new ArrayList<>();
-                    barEntries.add(new BarEntry(0, 6766));
-                    barEntries.add(new BarEntry(1, 4444));
-                    barEntries.add(new BarEntry(2, 2222));
-                    barEntries.add(new BarEntry(3, 5555));
-                    barEntries.add(new BarEntry(4, 1111));
-                    barEntries.add(new BarEntry(5, 3656));
-                    barEntries.add(new BarEntry(6, 3435));
-                    BarDataSet barDataSet = new BarDataSet(barEntries, "Steps");
-                    barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-                    ArrayList<IBarDataSet> dataSets = new ArrayList<>();
-                    List<String> xAxisValues = new ArrayList<>(Arrays.asList("Sun", "Mon", "Tues",
-                            "Wed", "Thurs", "Fri","Sat"));
-                    binding.barChart.getXAxis().setValueFormatter(new
-                            com.github.mikephil.charting.formatter.IndexAxisValueFormatter(xAxisValues));
-                    BarData barData = new BarData(barDataSet);
-                    binding.barChart.setData(barData);
-                    barData.setBarWidth(1.0f);
-                    binding.barChart.setVisibility(View.VISIBLE);
-                    binding.barChart.animateY(4000);
-                    Description description = new Description();
-                    description.setText("Daily Steps");
-                    binding.barChart.setDescription(description);
-                    binding.barChart.invalidate();
+                    generateBarChart();
                 } else {
-                    binding.barChart.setVisibility(View.INVISIBLE);
-                    binding.pieChart.setVisibility(View.VISIBLE);
-
-                    List<PieEntry> entries = new ArrayList<>();
-                    entries.add(new PieEntry(12.0f,"未违章"));
-                    entries.add(new PieEntry(88.0f,"违章"));
-
-                    binding.pieChart.setDrawHoleEnabled(true);
-
-                    String descriptionStr = "平台上有违章车辆和没违章车辆的占比统计";
-                    Description description = new Description();
-                    description.setText(descriptionStr);
-
-                    binding.pieChart.setExtraLeftOffset(0f);
-                    binding.pieChart.setExtraTopOffset(32f);
-                    binding.pieChart.setExtraRightOffset(0f);
-                    binding.pieChart.setExtraBottomOffset(32f);
-
-                    PieDataSet pieDataSet = new PieDataSet(entries,"违章情况");
-                    pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
-                    pieDataSet.setValueLinePart1OffsetPercentage(80f);
-                    pieDataSet.setSliceSpace(4f);
-                    pieDataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
-
-                    PieData pieData = new PieData(pieDataSet);
-                    pieData.setDrawValues(true);
-                    pieData.setValueFormatter(new PercentFormatter());
-                    binding.pieChart.setData(pieData);
-                    binding.pieChart.setDescription(description);
-
-                    binding.pieChart.invalidate();
-
-
+                    generatePieChart();
                 }
             }
         });
 
+    }
 
+    public void generateBarChart(){
+        binding.barChart.setVisibility(View.VISIBLE);
+        binding.pieChart.setVisibility(View.INVISIBLE);
+        List<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(0, 6766));
+        barEntries.add(new BarEntry(1, 4444));
+        barEntries.add(new BarEntry(2, 2222));
+        barEntries.add(new BarEntry(3, 5555));
+        barEntries.add(new BarEntry(4, 1111));
+        barEntries.add(new BarEntry(5, 3656));
+        barEntries.add(new BarEntry(6, 3435));
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Steps");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+        List<String> xAxisValues = new ArrayList<>(Arrays.asList("Sun", "Mon", "Tues",
+                "Wed", "Thurs", "Fri","Sat"));
+        binding.barChart.getXAxis().setValueFormatter(new
+                com.github.mikephil.charting.formatter.IndexAxisValueFormatter(xAxisValues));
+        BarData barData = new BarData(barDataSet);
+        binding.barChart.setData(barData);
+        barData.setBarWidth(1.0f);
+        binding.barChart.setVisibility(View.VISIBLE);
+        binding.barChart.animateY(4000);
+        Description description = new Description();
+        description.setText("Daily Steps");
+        binding.barChart.setDescription(description);
+        binding.barChart.invalidate();
+    }
 
+    public void generatePieChart(){
+        binding.barChart.setVisibility(View.INVISIBLE);
+        binding.pieChart.setVisibility(View.VISIBLE);
 
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(12.0f,"未违章"));
+        entries.add(new PieEntry(88.0f,"违章"));
+
+        binding.pieChart.setDrawHoleEnabled(true);
+
+        String descriptionStr = "平台上有违章车辆和没违章车辆的占比统计";
+        Description description = new Description();
+        description.setText(descriptionStr);
+
+        binding.pieChart.setExtraLeftOffset(0f);
+        binding.pieChart.setExtraTopOffset(32f);
+        binding.pieChart.setExtraRightOffset(0f);
+        binding.pieChart.setExtraBottomOffset(32f);
+
+        PieDataSet pieDataSet = new PieDataSet(entries,"违章情况");
+        pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        pieDataSet.setValueLinePart1OffsetPercentage(80f);
+        pieDataSet.setSliceSpace(4f);
+        pieDataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+
+        PieData pieData = new PieData(pieDataSet);
+        pieData.setDrawValues(true);
+        pieData.setValueFormatter(new PercentFormatter());
+        binding.pieChart.setData(pieData);
+        binding.pieChart.setDescription(description);
+
+        binding.pieChart.invalidate();
     }
 
 
